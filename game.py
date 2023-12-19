000d6fb939cf1cab822ce94c05ba7a8cbab49e7b
reeeverb <skydoessoftware@gmail.com>
2023-12-19 17:29:35 -0600

    switching things to game class

--- game.py
+++ game.py
@@ -2,6 +2,14 @@
 import pdb
 import random
 import board
+class Game():
+    def __init__(self,*args,**kwargs):
+        self.game_board = board.Board()
+        self.game_board.generate_random_board()
+        self.game_board.pp()
+        self.game_deck = board.Development_Cards()
+        self.turn_tree = 12*[[]]
+
 def main():
     main_board = board.Board() 
     main_board.generate_random_board()
@@ -9,20 +17,8 @@ def main():
     deck = board.Development_Cards()
     deck.shuffle()
     print(deck.draw_card())
+    game = Game()
     breakpoint()
 
 if __name__=="__main__":
     main()
-
-class Game():
-    def __init(self,*args,**kwargs):
-        game_board = board.Board()
-        game_board.generate_random_board()
-        game_board.pp()
-        game_deck = board.Development_Cards()
-        generate_turn_tree()
-        self.turn_tree = []
-    def generate_turn_tree():
-        for c in range(len(game_board.terrain)):
-            self.turn_tree[game_board.numbers[c]].append(c,game_board.terrain[c])
-
