67e89c83a9f4bad9ffa469747d661000f4a20b6c
reeeverb <skydoessoftware@gmail.com>
2023-12-19 16:30:41 -0600

    Dice and Development Cards

--- board.py
+++ board.py
@@ -1,8 +1,9 @@
 import random
 class Board():
-    terrains = (4*['Pasture']+4*['Forest']+4*['Fields']+3*['Hills']+3*['Mountains']+1*['Desert'])
-    numbers = ([2]+2*[3]+2*[4]+2*[5]+2*[6]+2*[8]+2*[9]+2*[10]+2*[11]+[12]+[0])
-    harbors = 4*["3"] + ["Brick"] + ["Lumber"] + ["Ore"] + ["Grain"] + ["Wool"]
+    def __init__(self, *args, **kwargs):
+        self.terrains = (4*['Pasture']+4*['Forest']+4*['Fields']+3*['Hills']+3*['Mountains']+1*['Desert'])
+        self.numbers = ([2]+2*[3]+2*[4]+2*[5]+2*[6]+2*[8]+2*[9]+2*[10]+2*[11]+[12]+[0])
+        self.harbors = 4*["3"] + ["Brick"] + ["Lumber"] + ["Ore"] + ["Grain"] + ["Wool"]
     def generate_random_board(self):
         # 4*Pasture, 4*Forest, 4*Fields, 3*Hills, 3*Mountains, 1*Desert
         # 1*2, 2*3, 2*4, 2*5, 2*6, 2*8, 2*9, 2*10, 2*11, 1*12
@@ -28,4 +29,51 @@ class Board():
         print(hgap,board[12],board[13],board[14],board[15])
         print()
         print(gap,board[16],board[17],board[18])
+
+class Development_Cards():
+    def __init__(self, *args, **kwargs):
+        self.reset_stack()
+    def reset_stack(a = None):
+        if a == None:
+            a = self
+        a.card_stack = 14*["Knight"]+5*["VP"]+2*["RB"]+2*["Monopoly"]
+        a.used_stack = []
+        a.shuffle()
+    def shuffle(a = None):
+        if a == None:
+            a = self 
+        random.shuffle(a.card_stack)
+        print(a.card_stack)
+    def draw_card(a = None):
+        if a == None:
+            a = self
+        if len(a.card_stack) == 0:
+            if len(a.used_stack) > 0:
+                print("RESHUFFLING")
+                a.card_stack = a.used_stack
+                a.shuffle()
+            else:
+                return None
+        return(a.card_stack.pop())
+    def card_used(card):
+        if card not in ["KNIGHT", "RB", "Monopoly"]:
+            print("INVALID CARD")
+        self.used_stack.append(card)
+
+class Dice():
+    def roll_one():
+        return random.randint(1,6)
+    def roll_two():
+        return random.randint(1,6) + random.randint(1,6)
+
+
+
+
+
+
+
+
+
+
+
 
--- game.py
+++ game.py
@@ -1,9 +1,21 @@
+#!/usr/bin/python3
+import pdb
 import random
 import board
 def main():
     main_board = board.Board() 
     main_board.generate_random_board()
     main_board.pp()
+    deck = board.Development_Cards()
+    deck.shuffle()
+    print(deck.draw_card())
+    breakpoint()
 
 if __name__=="__main__":
     main()
+
+class Game():
+    def __init(self,*args,**kwargs):
+        game_board = board.Board()
+        game_board.generate_random_board()
+        game_board.pp()
